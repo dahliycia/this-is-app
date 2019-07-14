33054b0d09d71799ec97159b89b4f987e5a2df4e
dahliycia <nnnnodahlia@gmail.com>
2019-07-14 15:59:35 +0200

    citation page

--- src/components/Thoughts/index.js
+++ src/components/Thoughts/index.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Grid, Typography, Paper, withStyles } from '@material-ui/core';
+
+
+const styles = {
+  root: {
+    padding: "20px"
+  },
+  paper: {
+    padding: "20px"
+  }
+}
+
 class Thoughts extends React.Component {
   render () {
+    const { classes } = this.props;
+
     return (
-      <div>
-        Thoiugh
+      <div className={classes.root}>
+        <Paper className={classes.paper}>
+          <Grid container direction="column" spacing={6}>
+            <Grid item>
+              <Typography variant="h4">
+                This is a beautiful thought
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sed viverra ipsum nunc aliquet bibendum enim. In massa tempor nec feugiat. Nunc aliquet bibendum enim facilisis gravida. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Amet luctus venenatis lectus magna fringilla. Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Sagittis orci a scelerisque purus semper eget duis. Nulla pharetra diam sit amet nisl suscipit. Sed adipiscing diam donec adipiscing tristique risus nec feugiat in. Fusce ut placerat orci nulla. Pharetra vel turpis nunc eget lorem dolor. Tristique senectus et netus et malesuada.
+                Etiam tempor orci eu lobortis elementum nibh tellus molestie. Neque egestas congue quisque egestas. Egestas integer eget aliquet nibh praesent tristique. Vulputate mi sit amet mauris. Sodales neque sodales ut etiam sit. Dignissim suspendisse in est ante in. Volutpat commodo sed egestas egestas. Felis donec et odio pellentesque diam. Pharetra vel turpis nunc eget lorem dolor sed viverra. Porta nibh venenatis cras sed felis eget. Aliquam ultrices sagittis orci a. Dignissim diam quis enim lobortis. Aliquet porttitor lacus luctus accumsan. Dignissim convallis aenean et tortor at risus viverra adipiscing at.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Paper>
       </div>
     )
   }
 }
 
-export default Thoughts;
+export default withStyles(styles)(Thoughts);
