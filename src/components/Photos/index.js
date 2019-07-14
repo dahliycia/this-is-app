493fcef55dd8dc69970beecc46ef93a8b0be9b60
dahliycia <nnnnodahlia@gmail.com>
2019-07-14 23:37:25 +0200

    added photo gallery

--- src/components/Photos/index.js
+++ src/components/Photos/index.js
@@ -1,27 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { GridList, GridListTile, GridListTileBar, ListSubheader, withStyles } from '@material-ui/core';
 
-import { fetchInfo } from './duck';
+import { infoFetched } from './duck';
+import getData from '../../fetch';
 
+
+const PHOTOS_NUMBER = 9;
+
+const styles = {
+  root: {
+    padding: "20px",
+  },
+  gridList: {
+    paddingLeft: "20%",
+    paddingRight: "20%",
+    width: "100%",
+  },
+}
 
 class Photos extends React.Component {
-  constructor(props) {
-    super(props);
-    if (!props.info) {
-      this.props.dispatch(fetchInfo())
-    }
-    console.log('CONSTRUCTOR')
-  }
   static propTypes = {
-    info: PropTypes.object
+    photos: PropTypes.array
+  }
+
+  fetchPhotosInfo = async (url) => {
+    const items = await getData(url).then(result => result.items || [])
+    this.props.dispatch(infoFetched(items.slice(0, PHOTOS_NUMBER)));
+  }
+  
+  componentDidMount = async () => {
+    const url = 'https://www.flickr.com/services/feeds/photos_public.gne?tags=marilynmonroe&format=json&nojsoncallback=?';
+    await this.fetchPhotosInfo(url)
+  }
+
+  handleClose = () => {
+    this.setState({open: false})
+  }
+
+  onPhotoClick = index => () => {
+    const { photos } = this.props;
+    const openImage = photos[index] || {};
+    const link = openImage.link;
+    if (link)
+      window.open(link)
   }
 
   render () {
-    console.log(this.props)
+    const { classes, photos } = this.props;
+
     return (
-      <div>
-        Photos
+      <div className={classes.root}>
+        <GridList cellHeight={200} className={classes.gridList} cols={3}>
+          <GridListTile key="Subheader" cols={3} style={{ height: 'auto' }}>
+            <ListSubheader component="div">Marilyn Monroe</ListSubheader>
+          </GridListTile>
+          {photos.map((item, index) => (
+            <GridListTile key={item.link} onClick={this.onPhotoClick(index)}>
+            <img src={item.media.m} alt={item.title} />
+            <GridListTileBar
+              title={item.title}
+              subtitle={<span>by: {item.author}</span>}
+            />
+          </GridListTile>
+          ))}
+        </GridList>
       </div>
     )
   }
@@ -31,4 +75,4 @@ const mapStateToProps = (state) => {
   return state.photos;
 }
 
-export default connect(mapStateToProps)(Photos);
+export default connect(mapStateToProps)(withStyles(styles)(Photos));
