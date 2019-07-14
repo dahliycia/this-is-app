c12320dd6876ced108472e97a23c41857062d816
dahliycia <nnnnodahlia@gmail.com>
2019-07-14 15:34:20 +0200

    initial with navigation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { AppBar, Tab, Tabs, MuiThemeProvider, CssBaseline, createMuiTheme } from '@material-ui/core'
+import { Feedback, Photo } from '@material-ui/icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Thoughts from './components/Thoughts';
+import Photos from './components/Photos';
+
+
+let theme = {
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#85C1E9'
+    },
+    secondary: {
+      main: '#2E86C1'
+    }
+  },
+}
+class App extends React.Component {
+  state = {
+    selectedTab: 0
+  }
+
+  handleChange = (event, newValue) => {
+    this.setState({selectedTab: newValue})
+  }
+
+  render () {
+    const { selectedTab } = this.state
+    return (
+      <div className="App">
+        <MuiThemeProvider theme={createMuiTheme(theme)} >
+          <Router>
+            <CssBaseline>
+              <AppBar position="static">
+                <Tabs value={selectedTab} onChange={this.handleChange}>
+                  <Tab label="Thoughts" icon={<Feedback />} component={Link} to={'/'} />
+                  <Tab label="Photos" icon={<Photo />} component={Link} to={'/photos'} />
+                </Tabs>
+              </AppBar>
+              <Route exact path="/" component={Thoughts} />
+              <Route exact path="/photos" component={Photos} />
+            </CssBaseline>
+          </Router>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Photos/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+class Photos extends React.Component {
+  render () {
+    return (
+      <div>
+        Photos
+      </div>
+    )
+  }
+}
+
+export default Photos;
